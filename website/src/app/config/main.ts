f0643c5ffcba3f583a21fdffbdd285ba97f83249
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2018-03-22 15:06:43 +0100

    htaccess und verschieben der proxy files

--- website/src/app/config/main.ts
+++ website/src/app/config/main.ts
@@ -639,5 +639,5 @@ export const VIDEOLIST = {
 };
 
 //Proxy-URL
-export const PROXY_URL = "http://localhost/WebPlayer/website/src/app/proxy/";
+export const PROXY_URL = "http://localhost/WebPlayer/website/src/proxy/";
 //export const PROXY_URL = "http://192.168.0.150/proxy/";
