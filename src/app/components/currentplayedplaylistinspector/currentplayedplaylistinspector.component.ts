ea0c09978b66eb3393c5ff051f7d519940eac8f0
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2018-05-18 17:36:53 +0200

    mobile Optik optimiert (hover)

--- src/app/components/currentplayedplaylistinspector/currentplayedplaylistinspector.component.ts
+++ src/app/components/currentplayedplaylistinspector/currentplayedplaylistinspector.component.ts
@@ -21,6 +21,9 @@ export class CurrentplayedplaylistinspectorComponent implements OnInit {
   //aktueller Index in Titelliste
   position: number;
 
+  //temp. Wert, wohin gerade gesprungen werden soll
+  jumpPosition: number = -1;
+
   //Service injecten
   constructor(private pls: PlaylistService, private bs: BackendService) { }
 
@@ -34,11 +37,25 @@ export class CurrentplayedplaylistinspectorComponent implements OnInit {
     this.files$ = this.bs.getFiles();
 
     //aktuellen Index in Titelliste abbonieren und in Variable schreiben (fuer CSS-Klasse)
-    this.bs.getPosition().subscribe(position => this.position = position);
+    this.bs.getPosition().subscribe(position => {
+      this.position = position;
+
+      //temp. Sprungwert (fuer optische Darstellung) wieder zuruecksetzen
+      this.jumpPosition = -1;
+    });
   }
 
   //zu gewissem Titel in Playlist springen
   jumpTo(position: number) {
+
+    //Befehl an WSS schicken
     this.bs.sendMessage({ type: "jump-to", value: position });
+
+    //Wenn zu einem anderen Titel gesprungen werden soll
+    if (this.position !== position) {
+
+      //bei diesem Eintrag einen Spinner anzeigen, bis der Titel geladen wurde
+      this.jumpPosition = position;
+    }
   }
 }
