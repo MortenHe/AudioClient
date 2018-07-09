08aacdc066128a891bb4fb3a90063092fe7132ef
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2018-07-09 15:38:26 +0200

    config für lokalen Windows WSS

---
+++ src/environments/environment.audio-win.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  envName: 'Audio (dev)',
+  production: false,
+  appMode: 'audio',
+  wssUrl: 'ws://127.0.0.1:8080',
+  proxyUrl: 'http://localhost/WebPlayer/src/proxy/',
+  domainModes: [{
+    "id": "hsp",
+    "label": "Hörspiele"
+  },
+  {
+    "id": "kindermusik",
+    "label": "Kindermusik"
+  },
+  {
+    "id": "musikmh",
+    "label": "Musik MH"
+  }]
+};
