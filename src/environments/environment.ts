738b0a2f3ca043ed0b8d1e4fd4ef4b0b856023f3
Martin Helfer <martin-helfer@gmx.de>
2020-03-05 21:09:41 +0100

    css

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,10 @@ export const environment = {
   envName: 'Steffis Player',
   assetId: 'pw',
   production: false,
-  serverUrl: 'http://132.230.25.79',
-  //serverUrl: 'http://localhost',
-  wssUrl: 'ws://132.230.25.79:8080',
-  //wssUrl: 'ws://localhost:8080',
+  //serverUrl: 'http://132.230.25.79',
+  serverUrl: 'http://localhost',
+  //wssUrl: 'ws://132.230.25.79:8080',
+  wssUrl: 'ws://localhost:8080',
   domainModes: [{
     "id": "hsp",
     "label": "HSP"
