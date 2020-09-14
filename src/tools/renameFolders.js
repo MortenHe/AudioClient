2d6865039695cadbebd14ee69b8c38573e04813c
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2020-09-14 15:44:05 +0200

    rename Folders mit slugify npm

--- src/tools/renameFolders.js
+++ src/tools/renameFolders.js
@@ -2,6 +2,9 @@
 
 //Zugriff auf Dateisystem
 const fs = require('fs-extra');
+const slugify = require('slugify')
+
+
 
 //Wo liegen die Ordner, die umbenannt werden sollen?
 const mediaDir = fs.readJsonSync("config.json").createAudioDir;
@@ -14,20 +17,11 @@ fs.readdirSync(mediaDir).forEach(folder => {
     const stat = fs.statSync(oldFolder);
     if (stat && stat.isDirectory()) {
 
-        //15 - Der rote Hahn -> 15 - der rote hahn
-        let newName = folder.toLowerCase();
-
-        //15 - der rote hahn -> 15-der rote hahn
-        newName = newName.replace(/ - /g, '-');
-
-        //15-der rote hahn -> 15-der-rote-hahn
-        newName = newName.replace(/ /g, '-');
-
-        //Umlaute aendern
-        newName = newName.replace(/ä/g, 'ae');
-        newName = newName.replace(/ö/g, 'oe');
-        newName = newName.replace(/ü/g, 'ue');
-        newName = newName.replace(/ß/g, 'ss');
+        //Slug erstellen: 15 - Der rote Hahn -> 15-der-rote-hahn
+        const newName = slugify(folder, {
+            lower: true,
+            locale: 'de'
+        });
 
         //Ordner umbenennen
         fs.renameSync(oldFolder, mediaDir + "/" + newName);
